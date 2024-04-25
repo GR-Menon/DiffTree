e696689fa93be5ba9017699a1db68fae09edc272
GR-Menon <menon.gautam.013@gmail.com>
2024-04-25 22:13:12 +0530

    updated logic

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import difflib as dl
 
-from diff import file_diff
+from txt2tree import txt2tree
 
+# Streamlit Elements
+
+# st.set_page_config(layout="wide")
 header = st.container()
 upload = st.container()
 output = st.container()
@@ -15,12 +18,13 @@ def text_diff(text1, text2):
     colored_diff = []
     for tag, i1, i2, j1, j2 in opcodes:
         if tag == 'replace':
-            colored_diff.append(f'<span style="color:blue">{text1[i1:i2]}</span>')
+            colored_diff.append(f'<span style="color:magenta">{text2[j1:j2]}</span>')
         elif tag == 'delete':
             colored_diff.append(f'<span style="color:red">{text1[i1:i2]}</span>')
         elif tag == 'insert':
             colored_diff.append(f'<span style="color:green">{text2[j1:j2]}</span>')
-
+        elif tag == 'equal':
+            colored_diff.append(text1[i1:i2])
     return ''.join(colored_diff)
 
 
@@ -32,18 +36,45 @@ with upload:
     pred_files = st.file_uploader("Predictions File", type=['txt'], accept_multiple_files=True)
 
 with output:
-    for pred in pred_files:
-        diff = file_diff(gt_file.name, pred.name)
-        st.subheader(pred.name)
-        st.write(diff)
+    gt_tree = txt2tree(gt_file.name)
+    if gt_file and pred_files:
+        for pred in pred_files:
+            col1, col2 = st.columns(2)
+            pred_tree = txt2tree(pred.name)
 
-        for idx, err in enumerate(diff):
-            gt = err[0]
-            pd = err[1]
-            if type(gt) != str:
-                st.write("Level Diff")
-                st.write(gt)
-            elif gt == pd:
-                st.write("Node_Type Diff")
-            elif gt != pd:
-                st.markdown(text_diff(gt, pd), unsafe_allow_html=True)
+            gt_nodes = pred_tree.keys() - gt_tree.keys()
+            pred_nodes = gt_tree.keys() - pred_tree.keys()
+            all_keys = sorted(gt_tree.keys() | pred_tree.keys())
+
+            with col1:
+                st.subheader(gt_file.name)
+                for key in all_keys:
+                    if key in gt_nodes:
+                        st.markdown(
+                            f'<span style="color:white">{key} </span><span style="color:cyan">x-x-x-x-x-x-x-x-x</span>',
+                            unsafe_allow_html=True)
+                    elif key not in gt_nodes:
+                        st.write(f"{key} - {gt_tree[key][0]} - {gt_tree[key][1]}")
+
+            with col2:
+                st.subheader(pred.name)
+                for key in all_keys:
+                    if key in pred_nodes:
+                        st.markdown(
+                            f'<span style="color:white">{key} </span><span style="color:cyan">x-x-x-x-x-x-x-x-x</span>',
+                            unsafe_allow_html=True)
+
+                    elif key not in gt_nodes and gt_tree[key][0] != pred_tree[key][0]:
+                        st.markdown(
+
+                            f'<span style="color:white">{key} - </span><span style="color:orange">{pred_tree[key][0]}</span><span style="color:white"> - {text_diff(gt_tree[key][1], pred_tree[key][1])}</span>',
+                            unsafe_allow_html=True)
+                    elif key not in gt_nodes:
+                        st.write(f'{key} - {pred_tree[key][0]} - {text_diff(gt_tree[key][1], pred_tree[key][1])}',
+                                 unsafe_allow_html=True)
+                    elif key in gt_nodes:
+                        st.write(f"{key} - {pred_tree[key][0]} - {pred_tree[key][1]}")
+
+            st.write(f"GT Nodes Missing: {gt_nodes}")
+            st.write(f"Prediction Nodes Missing: {pred_nodes}")
+            st.markdown("------------------------------")
--- txt2tree.py
+++ txt2tree.py
@@ -1,30 +1,17 @@
-from tree import TreeNode
-
-
 def txt2tree(file_path):
     with open(file_path, "r") as f:
         text = f.read()
     lines = text.strip().split("\n")
-    root = TreeNode("start")
-    stack = [(0, root)]
+    tree = {}
 
     for line in lines:
         tokens = line.split(' - ')
+        tree[tokens[0]] = (tokens[1], tokens[2])
 
-        depth = len(tokens[0].split('.'))
-        node_type = tokens[1]
-        data = tokens[2]
-
-        node = TreeNode(data, node_type)
-        while stack[-1][0] >= depth:
-            stack.pop()
-
-        stack[-1][1].add_child(node)
-        stack.append((depth, node))
-    return root
+    return tree
 
 
 if __name__ == "__main__":
-    file_path = "result_1.txt"
-    root = txt2tree(file_path)
-    root.print_tree()
+    file_path = "gtm_test.txt"
+    tree_dict = txt2tree(file_path)
+    print(tree_dict)
